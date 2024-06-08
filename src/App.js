bbc50dd66d4bef244ca0ea95e0f188865aaa8888
rajtirole <rtech23456@gmail.com>
2024-06-08 12:37:24 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 // App.js
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Backend_url from './constant'
+// const Backend_url = 'http://localhost:5000'; // Update to your backend URL
 
 const App = () => {
     const [accessToken, setAccessToken] = useState('');
     const [userData, setUserData] = useState(null);
-    const [reviews, setReviews] = useState([]);
 
     useEffect(() => {
         const urlParams = new URLSearchParams(window.location.search);
         const token = urlParams.get('access_token');
         const user = urlParams.get('user');
-        const reviewsData = urlParams.get('reviews');
 
-        if (token && user && reviewsData) {
+        if (token && user) {
             setAccessToken(token);
             setUserData(JSON.parse(decodeURIComponent(user)));
-            setReviews(JSON.parse(decodeURIComponent(reviewsData)));
         }
     }, []);
 
     const handleLogin = () => {
-        window.location.href = 'https://gserver5.onrender.com/login'; // Update to your backend URL
+        window.location.href = `${Backend_url}/login`;
     };
 
     return (
@@ -33,16 +33,6 @@ const App = () => {
                 <div>
                     <h2>User Data</h2>
                     <pre>{JSON.stringify(userData, null, 2)}</pre>
-                </div>
-            )}
-            {reviews.length > 0 && (
-                <div>
-                    <h2>Reviews</h2>
-                    {reviews.map((review, index) => (
-                        <div key={index}>
-                            <p><strong>{review.reviewer.displayName}:</strong> {review.comment}</p>
-                        </div>
-                    ))}
                 </div>
             )}
         </div>
